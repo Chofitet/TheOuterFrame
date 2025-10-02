5ce2d5dc381aa12427f5a7729ba795aa327f9949
Sofía Tetzlaff <sofiatetzlaff@gmail.com>
2025-10-02 10:54:40 -0300

    Screenshot, level alert to 99. No nodding and particles

---
+++ The Outer Frame/Assets/_Scripts/DebugTools/TakeScreenShoot.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeScreenShoot : MonoBehaviour
+{
+    int NumOfScreenshot = 0;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                // Resolución actual de la ventana de juego
+                int width = Screen.width;
+                int height = Screen.height;
+
+                string fileName = $"Screenshot_{width}x{height}_{NumOfScreenshot}.png";
+                ScreenCapture.CaptureScreenshot(fileName);
+
+                Debug.Log($"Screenshot guardado: {fileName}");
+                NumOfScreenshot += 1;
+            }
+        }
+    }
+}
