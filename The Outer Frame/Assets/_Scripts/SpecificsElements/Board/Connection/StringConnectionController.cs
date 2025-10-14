5d9fc83bf727233639770d192f90b59d80c6333f
Sebastian Parodi <parodi.seba@gmail.com>
2025-10-14 12:40:23 -0300

    All pics ON

--- The Outer Frame/Assets/_Scripts/SpecificsElements/Board/Connection/StringConnectionController.cs
+++ The Outer Frame/Assets/_Scripts/SpecificsElements/Board/Connection/StringConnectionController.cs
@@ -140,7 +140,10 @@ public class StringConnectionController : MonoBehaviour
         StartCoroutine(EnableMesh(true));
 
     }
-
+public void ConnectInStart(Component sender,object obj)
+ {
+     ConnectDirectly();
+ }
     IEnumerator EnableMesh(bool x)
     {
         yield return new WaitForSeconds(0.15f);
