3d7fac501a4c45cfc82b28af38aa2f0c378ba60e
Sofía Tetzlaff <sofiatetzlaff@gmail.com>
2025-11-06 20:07:52 -0300

    update stuck fix?

--- The Outer Frame/Assets/_Scripts/SpecificsElements/Board/Photo/BoardNodeController.cs
+++ The Outer Frame/Assets/_Scripts/SpecificsElements/Board/Photo/BoardNodeController.cs
@@ -42,7 +42,7 @@ public class BoardNodeController : MonoBehaviour, IPlacedOnBoard
         else return false;
     }
 
-        void ActiveChildPosits()
+        /*void ActiveChildPosits()
     {
         foreach(MoveBoardElementsToPos posit in PostIts)
         {
@@ -55,7 +55,7 @@ public class BoardNodeController : MonoBehaviour, IPlacedOnBoard
                 Canvas.SetActive(false);
             }
         }
-    }
+    }*/
 
     bool CheckRetroactiveWordWasPlaced() 
     {
