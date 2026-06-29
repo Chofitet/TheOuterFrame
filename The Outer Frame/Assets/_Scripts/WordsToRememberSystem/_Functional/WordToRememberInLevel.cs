9b7817a1e681b16a36579aa8948a9f1033b2cfe4
Sofía Tetzlaff <sofiatetzlaff@gmail.com>
2026-06-29 10:40:47 -0300

    Curve text and sounds for member words in level.

--- The Outer Frame/Assets/_Scripts/WordsToRememberSystem/_Functional/WordToRememberInLevel.cs
+++ The Outer Frame/Assets/_Scripts/WordsToRememberSystem/_Functional/WordToRememberInLevel.cs
@@ -10,20 +10,22 @@ public class WordToRememberInLevel : MonoBehaviour
     WordData word;
     [SerializeField] TMP_Text textField;
     [SerializeField] List<GameObject> models = new List<GameObject>();
+    [SerializeField] List<TMP_Text> texts = new List<TMP_Text>();
+    [SerializeField] GameEvent TakeDownMemberWordSound;
     public void Initialize(WordData _word)
     {
         word = _word;
-        textField.text = _word.GetName();
 
         foreach (GameObject model in models) { model.SetActive(false); }
         models[word.GetMemberWordNumPaperModel()].SetActive(true);
+        texts[word.GetMemberWordNumPaperModel()].text = _word.GetName();
     }
 
     public void GetOut(Component sender, object obj)
     {
         if ((WordData)obj != word) return;
-       
 
+        TakeDownMemberWordSound?.Invoke(this, null);
         Vector3 finalPos = transform.position + new Vector3(0,-0.05f,0);
         transform.DOMove(finalPos, 0.5f).OnComplete(()=> Destroy(gameObject));
     }
