d50299d9b7e2ec826eb743a2ab3ca508f7b5a47f
Sofía Tetzlaff <sofiatetzlaff@gmail.com>
2026-07-13 13:00:59 -0300

    words fade

--- The Outer Frame/Assets/_Scripts/SpecificsElements/Notebook/Operative/NotebookPage.cs
+++ The Outer Frame/Assets/_Scripts/SpecificsElements/Notebook/Operative/NotebookPage.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class NotebookPage : MonoBehaviour
 {
@@ -11,7 +12,10 @@ public class NotebookPage : MonoBehaviour
     int numWords;
     List<Transform> WordSpots = new List<Transform>();
     bool isFull;
-    [SerializeField]int PassToSecondColumn;
+    [SerializeField] int PassToSecondColumn;
+    [SerializeField] RectTransform rect;
+    [SerializeField] float ShowWordsDuration;
+    [SerializeField] float HideWordsDuration;
 
     private void Start()
     {
@@ -24,7 +28,10 @@ public class NotebookPage : MonoBehaviour
 
     public void DisableEnable(bool x)
     {
-        gameObject.GetComponent<Canvas>().enabled = x;
+        //gameObject.GetComponent<Canvas>().enabled = x;
+
+        if (x) AnimateMask(110, ShowWordsDuration,0.1f);
+        else AnimateMask(0, HideWordsDuration,0);
     }
 
     public List<Transform> GetWordSpots() { return WordSpots; }
@@ -34,13 +41,25 @@ public class NotebookPage : MonoBehaviour
 
     public GameObject InstanciateWord()
     {
-        GameObject wordaux = Instantiate(WordPrefab, WordSpots[numWords].position, WordSpots[numWords].rotation, transform);
+        GameObject wordaux = Instantiate(WordPrefab, WordSpots[numWords].position, WordSpots[numWords].rotation, rect.gameObject.transform);
         numWords++;
         if (PassToSecondColumn < numWords) wordaux.GetComponent<NotebookWordInstance>().IsInSecondColumn();
         if (numWords == WordAnchors.childCount) isFull = true;
         return wordaux;
     }
-    
+
+    public Sequence AnimateMask(float targetWidth, float duration, float startingDelay)
+    {
+        return DOTween.Sequence().PrependInterval(startingDelay).Append(
+            DOTween.To(
+                () => rect.sizeDelta.x,
+                x => rect.sizeDelta = new Vector2(x, rect.sizeDelta.y),
+                targetWidth,
+                duration
+            )
+        );
+    }
+
 }
 
 
