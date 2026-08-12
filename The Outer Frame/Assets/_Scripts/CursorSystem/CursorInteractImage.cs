1486c0066507678f9b9d865d1ebef1a3e48eb6d0
Sofía Tetzlaff <sofiatetzlaff@gmail.com>
2026-08-12 11:56:53 -0300

    Settings

---
+++ The Outer Frame/Assets/_Scripts/CursorSystem/CursorInteractImage.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CursorInteractImage : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
+{
+    private Image img;
+
+    private bool isInside;
+    private bool wasValid;
+    [SerializeField] bool isAlwaysValid;
+
+    void Awake()
+    {
+        img = GetComponent<Image>();
+        wasValid = IsValid();
+    }
+
+    bool IsValid()
+    {
+        return gameObject.activeInHierarchy && img != null;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!IsValid()) return;
+
+        isInside = true;
+        CursorManager.CM.EnterInteractive();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!isInside) return;
+
+        isInside = false;
+        CursorManager.CM.ExitInteractive();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!isInside) return; 
+        CursorManager.CM.ClickInteractive();
+    }
+
+    void Update()
+    {
+        bool valid = IsValid();
+
+        // Si el botón dejó de ser interactuable mientras el cursor estaba encima,
+        // simulamos un PointerExit.
+        if (wasValid && !valid && isInside && !isAlwaysValid)
+        {
+            isInside = false;
+            CursorManager.CM.ExitInteractive();
+        }
+
+        wasValid = valid;
+    }
+
+    private void OnDisable()
+    {
+        if (!isInside) return;
+
+        //primero este
+        Invoke("MarkInsideFalse", 0.01f);
+        CursorManager.CM.ExitInteractive();
+    }
+
+    void MarkInsideFalse()
+    {
+        isInside = false;
+    }
+
+}
--- The Outer Frame/Assets/_Scripts/_Functional/Settings/SettingsManager.cs
+++ The Outer Frame/Assets/_Scripts/_Functional/Settings/SettingsManager.cs
@@ -1,33 +1,122 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
-    [SerializeField] DropdownField qualityDropdown;
-    int quality;
-
-    public void ShowMenu()
+    void Start()
     {
-        transform.GetChild(0).gameObject.SetActive(true);
+        SetResolutionDrowDown();
+
+        IsFullScreen = Screen.fullScreen;
+        FullScreenToggle.isOn = IsFullScreen;
     }
 
-    public void ConfirmBtn()
+    public void ShowMenu(Component sender, object obj)
     {
+        VisivilityOfMenu(true);
+    }
+
+    public void VisivilityOfMenu(bool x)
+    {
+        Menu.SetActive(x);
+    }
+
+    public void ApplyBtn()
+    {
+        ConfirmResolution();
         ConfirmQuality();
+        VisivilityOfMenu(false);
     }
 
-    void SetQuality(int qualityIndex)
+    public void BackBTN()
     {
-        Debug.Log($"Set quality: {qualityIndex}");
-        quality = qualityIndex;
-        
+        VisivilityOfMenu(false);
+    }
+
+    ///------------------------ Quality ------------------------------------///
+    [SerializeField] bool EnableQualityOp = true;
+    [SerializeField] TMP_Dropdown qualityDropdown;
+    [SerializeField] GameObject Menu;
+    int quality = 5;
+
+    public void SetQuality()
+    {
+        if (!EnableQualityOp) return;
+        quality = qualityDropdown.value;
+        Debug.Log($"Set quality: {quality}");
+
     }
 
     void ConfirmQuality()
     {
+        if (!EnableQualityOp) return;
         QualitySettings.SetQualityLevel(quality, true);
-        transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+
+    ///------------------------ Resolution ------------------------------------///
+
+    [SerializeField] TMP_Dropdown ResDropDown;
+    [SerializeField] Toggle FullScreenToggle;
+    Resolution[] AllResolutions;
+    bool IsFullScreen;
+    int SelectedResolution;
+    List<Resolution> SelectedResolutionList = new List<Resolution>();
+
+    void SetResolutionDrowDown()
+    {
+        IsFullScreen = true;
+        AllResolutions = Screen.resolutions;
+
+        List<string> resolutionStringList = new List<string>();
+        string newRes;
+
+        int currectResIndex = 0;
+        int index = 0;
+        foreach (Resolution res in AllResolutions)
+        {
+            newRes = res.width.ToString() + " x " + res.height.ToString();
+
+            if (!resolutionStringList.Contains(newRes))
+            {
+                resolutionStringList.Add(newRes);
+                SelectedResolutionList.Add(res);
+            }
+
+            if(res.width == Screen.currentResolution.width  && res.height == Screen.currentResolution.height)
+            {
+                currectResIndex = index;
+            }
+
+            index++;
+        }
+        ResDropDown.AddOptions(resolutionStringList);
+        ResDropDown.value = currectResIndex;
+        ResDropDown.RefreshShownValue();
+    }
+
+    public void SetResolution()
+    {
+        SelectedResolution = ResDropDown.value;
+        ResDropDown.RefreshShownValue();
+
+    }
+
+    public void SetFullScreen()
+    {
+        IsFullScreen = FullScreenToggle.isOn;
+
+    }
+
+    void ConfirmResolution()
+    {
+        Screen.SetResolution(
+            SelectedResolutionList[SelectedResolution].width,
+            SelectedResolutionList[SelectedResolution].height,
+            IsFullScreen
+        );
     }
 }
