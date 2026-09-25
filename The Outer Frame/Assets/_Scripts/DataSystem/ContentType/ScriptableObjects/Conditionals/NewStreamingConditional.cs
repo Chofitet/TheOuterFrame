939f780c73b134ed89a3dbb0630cac4f865672d0
Sofía Tetzlaff <sofiatetzlaff@gmail.com>
2026-09-25 12:39:00 -0300

    No weird shadows on report photos

--- The Outer Frame/Assets/_Scripts/DataSystem/ContentType/ScriptableObjects/Conditionals/NewStreamingConditional.cs
+++ The Outer Frame/Assets/_Scripts/DataSystem/ContentType/ScriptableObjects/Conditionals/NewStreamingConditional.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Streaming Conditional", menuName = "Conditionals/New Streaming Conditional")]
+[CreateAssetMenu(fileName = "was broadcasted conditional", menuName = "Conditionals/Was broadcasted conditional")]
 public class NewStreamingConditional : ScriptableObject, IConditionable
 {
     [SerializeField] ScriptableObject New;
 
+    bool wasStremed;
     public bool CheckIfHaveTime()
     {
         return false;
@@ -23,7 +24,9 @@ public class NewStreamingConditional : ScriptableObject, IConditionable
     {
         if (New is not INewType) Debug.LogWarning("The conditional " + name + " have a wrong type of Scriptable Object assigned in a field");
         INewType _new = New as INewType;
-        
+
+        wasStremed = _new.GetWasStreamed();
+
         return _new.GetWasStreamed();
     }
 
